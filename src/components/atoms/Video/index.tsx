c6f010eed21613aff3b3d922e2ac7fea094f2ac5
Henrique <henriqueferreira.fox@gmail.com>
2024-01-30 18:18:26 -0300

    add video

--- src/components/atoms/Video/index.tsx
+++ src/components/atoms/Video/index.tsx
@@ -1,11 +1,24 @@
 import { Box, Paper } from "@mui/material";
 
 export default function Video() {
-    return(
-        <Box sx={{display: "flex", justifyContent: "center", width: "100%", marginY: 8}}>
-            <Paper sx={{width: "80%"}} elevation={24}>
-                <video style={{width: "100%", height: "100%"}} controls />
-            </Paper>
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        width: "100%",
+        marginY: 8,
+      }}
+    >
+      <Paper sx={{ width: "80%" }} elevation={24}>
+        <iframe
+          style={{ width: "100%" }}
+          width="560"
+          height="515"
+          src={"//www.youtube.com/watch?v=NCVpvfdfre4"}
+          allowFullScreen
+        ></iframe>
+      </Paper>
+    </Box>
+  );
 }
